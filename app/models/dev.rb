f699f9b5657ae76ed7c2442f9aced8c85fc72e7b
Sid Saythongphet <sid.saythongphet@gmail.com>
2022-02-12 17:16:41 -0500

    commit tree 8b1e1e53c5636d4390c0b66d5014b259a213a9b9

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,17 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
+
+    def received_one? item_name
+        self.freebies.map{ |freebie| freebie.item_name }.include?(item_name)
+    end
+
+    def give_away dev, freebie
+        if freebie.dev == self
+            freebie.update(:dev => dev)
+        else
+            puts "I don't own it"
+        end
+    end
 
 end
